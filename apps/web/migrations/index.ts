b850419ea5e18536becd4967918740ebc7bb90d9
Johannes Filter <hi@jfilter.de>
2025-07-19 20:45:38 +0200

    fix(db): resolve missing migrations causing GitHub Actions build failure

--- apps/web/migrations/index.ts
+++ apps/web/migrations/index.ts
@@ -2,6 +2,10 @@ import * as migration_20250718_145739 from "./20250718_145739";
 import * as migration_20250718_add_events_location_gist from "./20250718_add_events_location_gist";
 import * as migration_20250718_225716_add_clustering_function from "./20250718_225716_add_clustering_function";
 import * as migration_20250718_225751_add_histogram_function from "./20250718_225751_add_histogram_function";
+import * as migration_20250719_update_clustering_distance from "./20250719_update_clustering_distance";
+import * as migration_20250719_fix_histogram_function from "./20250719_fix_histogram_function";
+import * as migration_20250719_fix_histogram_timezone from "./20250719_fix_histogram_timezone";
+import * as migration_20250719_update_histogram_function from "./20250719_update_histogram_function";
 
 export const migrations = [
   {
@@ -23,5 +27,25 @@ export const migrations = [
     up: migration_20250718_225751_add_histogram_function.up,
     down: migration_20250718_225751_add_histogram_function.down,
     name: "20250718_225751_add_histogram_function",
+  },
+  {
+    up: migration_20250719_update_clustering_distance.up,
+    down: migration_20250719_update_clustering_distance.down,
+    name: "20250719_update_clustering_distance",
+  },
+  {
+    up: migration_20250719_fix_histogram_function.up,
+    down: migration_20250719_fix_histogram_function.down,
+    name: "20250719_fix_histogram_function",
+  },
+  {
+    up: migration_20250719_fix_histogram_timezone.up,
+    down: migration_20250719_fix_histogram_timezone.down,
+    name: "20250719_fix_histogram_timezone",
+  },
+  {
+    up: migration_20250719_update_histogram_function.up,
+    down: migration_20250719_update_histogram_function.down,
+    name: "20250719_update_histogram_function",
   },
 ];
