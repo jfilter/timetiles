012e693ffe1655b4226e7a6d0ea28e3cba4d4dc1
Johannes Filter <hi@jfilter.de>
2025-11-23 01:09:19 +0100

    docs: condense verbose documentation files
    - Reduce assets.mdx from 200 to 42 lines (79% reduction)
    - Remove verbose Git LFS setup instructions (handled by make setup)
    - Merge git-configuration.mdx into commit-guidelines.mdx
    - Update _meta.js to reflect merged docs

--- apps/docs/content/developer-guide/development/_meta.js
+++ apps/docs/content/developer-guide/development/_meta.js
@@ -2,6 +2,5 @@ export default {
   "documentation-guide": "Documentation Guide",
   "testing-guidelines": "Testing Guidelines",
   "payload-deadlocks": "Payload CMS Deadlock Prevention",
-  "commit-guidelines": "Commit Guidelines",
-  "git-configuration": "Git Configuration",
+  "commit-guidelines": "Git & Commit Guidelines",
 };
