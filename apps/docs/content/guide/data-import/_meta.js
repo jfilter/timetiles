543b289fc07e781a3e594ad1770c0d1ff815f1de
Johannes Filter <hi@jfilter.de>
2026-04-01 00:32:44 +0200

    docs: add data packages guide with FtM compatibility

--- apps/docs/content/guide/data-import/_meta.js
+++ apps/docs/content/guide/data-import/_meta.js
@@ -1,6 +1,7 @@
 export default {
   "file-upload": "File Upload",
   "scheduled-imports": "Scheduled Imports",
+  "data-packages": "Data Packages",
   scrapers: "Scrapers",
   transforms: "Transforms",
 };
